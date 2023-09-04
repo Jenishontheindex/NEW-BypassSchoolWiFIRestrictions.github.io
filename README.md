aeac05ef22f03e47de45f77fd18b6253629e9041
Jenishontheindex <46525717+Jenishontheindex@users.noreply.github.com>
2023-09-04 21:41:01 +0930

    Uploaded files
    Uploaded the files after exporting from replit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bypass
+#### Bypass the school wifi restrictions
+##### Open in your browser [here](https://yeshivaschools.github.io/bypass/bypass.html).
---
+++ bypass.js
@@ -0,0 +1,28 @@
+const information = get("information");
+const data = get("data");
+
+window.addEventListener("load", () => prompt("Password") === "aroary" ? show(get("intro")) : [document.body.outerHTML = "", alert("incorrect password")]);
+
+get("ok").addEventListener("click", () => information.style.display = "none");
+get("info").addEventListener("click", () => show(get("infoData")));
+
+/**
+ * @description get an element by id
+ * @param {string} id 
+ * @returns {Element} the element with the given id
+ */
+function get(id) {
+    return document.getElementById(id);
+};
+
+/**
+ * @description Show premade content in the information window
+ * @param {Element} template 
+ */
+function show(template) {
+    data.replaceChildren(template.content.cloneNode(true));
+    information.style.display = "block";
+};
+
+// 'The servers running your browser is run by <a href="https://replit.com" target="_blank" rel="noopener noreferrer">Replit</a>.',
+// "Firefox is set as the defualt browser because chrome has more filters and can sometimes fail to show you what you want to see."
